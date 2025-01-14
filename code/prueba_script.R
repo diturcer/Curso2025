e0121eeecde55ffbbcde71ab1ee67d0589eadb80
Diana <diana.turrion@ua.es>
2025-01-14 16:22:55 +0100

    plot_ancho_sepalo_dist

--- code/prueba_script.R
+++ code/prueba_script.R
@@ -8,3 +8,6 @@ library(ggplot2)
 ggplot(dt, 
        aes(x=Petal.Length, fill=Species))+
   geom_density(alpha=0.5)
+ggplot(dt, 
+       aes(x=Sepal.Width , fill=Species))+
+  geom_density(alpha=0.5)
